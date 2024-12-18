7f27e0c3b0597e8c64834c47590c5204a5bdb04c
highly unskilled <141437346+paradiseofmadness@users.noreply.github.com>
2024-12-18 18:34:00 +0100

    Update b7_a3_p1.asm

--- b7_a3_p1.asm
+++ b7_a3_p1.asm
@@ -2,17 +2,17 @@
             .globl programm1        // "programm1" extern sichtbar
 programm1:  SUBI SP, SP, #8         // stack frame anlegen
             STUR LR, [SP, #0]       // Rücksprungadresse retten
-            LDUR XS0, x             // XS0 = x
-            LDUR XS1, div           // XS1 = div
+            LDUR A0, x             // A0 = x
+            LDUR A1, div           // A1 = div
             BL loop                 // ruft die Schleife auf
-            STUR XS0, x             // schreibt das neue x zurück
+            STUR A0, x             // schreibt das neue x zurück
             LDUR LR, [SP, #0]       // Rücksprungadresse restaurieren
             ADDI SP, SP, #8         // stack frame löschen
             BR LR                   // Rücksprung
                                     
-loop:       CMP XS0, XS1            
-            B.LT ende               // Abbruch bei XS0 < XS1 (x < div)
-            SUB XS0, XS0, XS1       // XS0 -= XS1 (x -= div)
+loop:       CMP A0, A1            
+            B.LT ende               // Abbruch bei A0 < A1 (x < div)
+            SUB A0, A0, A1       // A0 -= A1 (x -= div)
             B loop                  // zurück zum Schleifenanfang
                                     
 ende:       BR LR                   // Rücksprung in "programm1"
