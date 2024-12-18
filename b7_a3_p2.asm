8b776b8d665d24c0a1c62235fce958a0e8f05e93
highly unskilled <141437346+paradiseofmadness@users.noreply.github.com>
2024-12-18 18:34:26 +0100

    Update b7_a3_p2.asm

--- b7_a3_p2.asm
+++ b7_a3_p2.asm
@@ -2,20 +2,20 @@
             .globl programm2        // "programm2" extern sichtbar
 programm2:  SUBI SP, SP, #8         // stack frame anlegen
             STUR LR, [SP, #0]       // Rücksprungadresse retten
-            LDUR XS0, input         // XS0 = input
-            LDUR XS1, pin           // XS1 = pin
-            ADD XS2, XZR, XZR       // XS2 =  trail = 0
+            LDUR A0, input         // A0 = input
+            LDUR A1, pin           // A1 = pin
+            ADD A2, XZR, XZR       // A2 =  trail = 0
             BL loop                 // ruft die Schleife auf
             LDUR LR, [SP, #0]       // Rücksprungadresse restaurieren
             ADDI SP, SP, #8         // stack frame löschen
             BR LR                   // Rücksprung
 
-loop:       ADDI XS0, XS0, #10      // Erhöht XS0 / input um 10
-            ADDI XS2, XS2, #1       // Erhöht XS2 / trail um 1
-            CMP XS0, XS1
-            B.EQ ende               // Abbruch wenn XS0 == XS1 (input == pin)
-            CMP XS2, #4
-            b.EQ ende               // Abbruch wenn XS2 == 4 (trail == 4)
+loop:       ADDI A0, A0, #10      // Erhöht A0 / input um 10
+            ADDI A2, A2, #1       // Erhöht A2 / trail um 1
+            CMP A0, A1
+            B.EQ ende               // Abbruch wenn A0 == A1 (input == pin)
+            CMP A2, #4
+            b.EQ ende               // Abbruch wenn A2 == 4 (trail == 4)
             B loop                  // Zurück zum Schleifenbeginn (wenn input!=pin && trial!=4)
 
 ende:       BR LR                   // Rücksprung in "programm2" 
